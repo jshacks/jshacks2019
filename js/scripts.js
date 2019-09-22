f6fadee76ac6ed2d66e8efa1da6cede11debaef0
Diana Miron <diana@ap3.ro>
2019-09-22 14:55:42 +0300

    2019 website yaaas

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,66 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $('a[href*="#"]')
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .click(function (event) {
+    .click(function(event) {
       // On-page links
       if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
         location.hostname == this.hostname
       ) {
         // Figure out element to scroll to
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
         // Does a scroll target exist?
         if (target.length) {
           // Only prevent default if animation is actually gonna happen
           event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function () {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000,
+            function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
         }
       }
     });
 
   // Opera 8.0+
-  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  var isOpera =
+    (!!window.opr && !!opr.addons) ||
+    !!window.opera ||
+    navigator.userAgent.indexOf(" OPR/") >= 0;
 
   // Firefox 1.0+
-  var isFirefox = typeof InstallTrigger !== 'undefined';
+  var isFirefox = typeof InstallTrigger !== "undefined";
 
-  // Safari 3.0+ "[object HTMLElementConstructor]" 
-  var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+  // Safari 3.0+ "[object HTMLElementConstructor]"
+  var isSafari =
+    navigator.vendor &&
+    navigator.vendor.indexOf("Apple") > -1 &&
     navigator.userAgent &&
-    navigator.userAgent.indexOf('CriOS') == -1 &&
-    navigator.userAgent.indexOf('FxiOS') == -1;
+    navigator.userAgent.indexOf("CriOS") == -1 &&
+    navigator.userAgent.indexOf("FxiOS") == -1;
 
   // Internet Explorer 6-11
-  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  var isIE = /*@cc_on!@*/ false || !!document.documentMode;
 
   // Edge 20+
   var isEdge = !isIE && !!window.StyleMedia;
@@ -60,15 +72,13 @@ $(document).ready(function () {
   var isBlink = (isChrome || isOpera) && !!window.CSS;
 
   function removeRellax() {
-    var rellaxobj = document.getElementsByClassName('rellax');
+    var rellaxobj = document.getElementsByClassName("rellax");
     while (rellaxobj[0]) {
-      rellaxobj[0].classList.remove('rellax');
+      rellaxobj[0].classList.remove("rellax");
     }
   }
 
   if (isFirefox == true || isSafari == true || isEdge == true || isIE == true) {
     removeRellax();
   }
-
-
 });
